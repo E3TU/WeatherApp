f447fc051e5e02402e1b9319b3630fa61bd06d65
Eetu <99178022+E3TU@users.noreply.github.com>
2023-12-16 00:19:55 +0200

    Updated scraper

--- server.js
+++ server.js
@@ -1,31 +1,35 @@
 import puppeteer from "puppeteer";
 
 async function getWeatherData() {
+  const browser = await puppeteer.launch({ headless: 'new' });
+  const page = await browser.newPage();
 
-    const browser = await puppeteer.launch({ headless: 'new' });
-    const page = await browser.newPage();
-  
-    try {
-      // Navigate to the weather page
-      await page.goto('https://www.foreca.fi/Finland/Helsinki');
-  
-      // Extract the weather data from the page
-      const weatherData = await page.evaluate(() => {
-        // Customize this based on the actual structure of the website
-        const temperature = document.querySelector('.temp').innerText;
-  
-        return {
-          temperature
-        };
-      });
-  
-      console.log('Weather Data:', weatherData);
-    } catch (error) {
-      console.error('Error scraping data:', error.message);
-    } finally {
-      // Close the browser
-      await browser.close();
+  try {
+    // Navigate to the weather page
+    await page.goto('https://www.foreca.fi/Finland/Helsinki');
+
+    // Extract the weather data from the page
+    const coldElement = await page.$('.t.cold');
+    const warmElement = await page.$('.t.warm');
+
+    const weatherData = {};
+
+    if (coldElement) {
+      weatherData.cold = await page.evaluate(el => el.innerText, coldElement);
     }
+
+    if (warmElement) {
+      weatherData.warm = await page.evaluate(el => el.innerText, warmElement);
+    }
+
+    console.log('Weather Data:', weatherData);
+  } catch (error) {
+    console.error('Error scraping data:', error.message);
+  } finally {
+    // Close the browser
+    await browser.close();
+  }
 }
-  
+
+// Call the function
 getWeatherData();
