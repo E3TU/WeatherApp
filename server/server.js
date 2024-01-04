32bf5742d09c32571bceb81ee75f167838fc98fb
Eetu <99178022+E3TU@users.noreply.github.com>
2024-01-04 23:40:33 +0200

    Renamed puppeteer script import

--- server/server.js
+++ server/server.js
@@ -1,17 +1,18 @@
 import express from "express";
 import cors from "cors";
-import getWeatherData from "../scripts/puppeteer-scraper.js";
+import puppeteerScraper from "../scripts/puppeteer-scraper.js";
 
 const app = express();
 // Define what port to use
 const port = 3001;
 
 app.use(cors());
+app.use(express.json());
 
 app.get("/weather", async (req, res) => {
     try {
         // Call the getWeatherData function from puppeteer-scraper
-        const weatherData = await getWeatherData();
+        const weatherData = await puppeteerScraper();
         // Send the weatherData as response
         res.json({ weatherData });
     } catch (error) {
